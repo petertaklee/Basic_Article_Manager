81b9cdf98c61bc2e6e2039bd7afa07174148eb8f
petertaklee <petertaklee3@gmail.com>
2024-07-12 20:51:07 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
 
+		Scanner sc = new Scanner(System.in);
+		// test
 		while (true) {
 			System.out.printf("명령여)");
 
-			Scanner sc = new Scanner(System.in);
 
 			String cmd = sc.nextLine();
 
 			System.out.println("입력된 몀령어 :" + cmd);
 
 			if (cmd.equals("exit")) {
-
+                    break;
 			}
 		}
 
-		System.out.println("== 프 로그램 끝 ==");
+		System.out.println("== 프로그램 끝 ==");
 
 	}
 }
