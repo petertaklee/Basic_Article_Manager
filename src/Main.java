cd35231d4de48810ffc016f9cf235a9a7c2a11ee
petertaklee <petertaklee3@gmail.com>
2024-07-15 19:51:01 +0900

    수정

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-
+		
 		Scanner sc = new Scanner(System.in);
-		// test
+		
 		while (true) {
-			System.out.printf("명령여)");
-
-
+			System.out.printf("명령어) ");
+			
 			String cmd = sc.nextLine();
-
-			System.out.println("입력된 몀령어 :" + cmd);
-
+			
+			System.out.println("입력된 명령어 : " + cmd);
+		
 			if (cmd.equals("exit")) {
-                    break;
+				break;
 			}
 		}
-
+		
+		sc.close();
+		
 		System.out.println("== 프로그램 끝 ==");
-
 	}
 }
